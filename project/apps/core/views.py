d7e436acd66d984bfbf2a61f265e6a89e0a98f5c
Abhishek Sharma <abhishek@loansimple.in>
2020-04-02 15:21:30 +0530

    small bug fixes

--- project/apps/core/views.py
+++ project/apps/core/views.py
@@ -17,7 +17,16 @@ from core.serializers import EmailEventSerializer, CustomerSerializer, EmailSeri
 @api_view(['GET'])
 def mark_read_email(request):
     if request.GET.get('eid'):
-        Email.objects.select_for_update().filter(tracking_id=request.GET['eid']).update(is_seen=True)
+        email = Email.objects.get(tracking_id=request.GET.get('eid'))
+        d = {
+            'created_at': utility.get_current_time(),
+            'email': email.id,
+            'type': EMAIL_OPEN_EVENT[0]
+        }
+        ser = EmailEventSerializer(data=d)
+        ser.is_valid(raise_exception=True)
+        ser.save()
+        return HttpResponse({'message': 'Thank you for using Finshots.'}, status=200)
     red = Image.new('RGBA', (1, 1), (255, 0, 0, 0))
     res = HttpResponse(content_type="image/png")
     red.save(res, "JPEG")
